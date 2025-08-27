8849b1c9538cf2e1ffecf0e74dc51369a2ff3eaf
NSavage13 <savagenico@yahoo.com>
2025-08-26 20:29:43 -0400

    Update Database.java

--- src/db/Database.java
+++ src/db/Database.java
@@ -6,7 +6,7 @@ public class Database {
 
 private static final String URL = "jdbc:mysql://localhost:3306/job_tracker";
     private static final String USERNAME = "root";
-    private static final String PASSWORD = ""; // Don't forget to hide this
+    private static final String PASSWORD = "Micheal13"; // Don't forget to hide this
 
     public static Connection connect() throws SQLException {
         return DriverManager.getConnection(URL, USERNAME, PASSWORD);
